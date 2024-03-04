83eb1aa44ff8a1db3cf29bac1b8e81a06c30ba6c
Deepak Vishwakarma <coder.deepak44@gmail.com>
2024-03-04 23:31:59 +0530

    Print Alphabets: A Java program that prints lowercase alphabets from 'a' to 'z'. 🔄🔠 #JavaProgramming #Alphabets

---
+++ PrintAlphabets.java
@@ -0,0 +1,9 @@
+public class PrintAlphabets {
+    public static void main(String[] args) {
+        // Print lowercase alphabets from 'a' to 'z'
+        System.out.println("Alphabets from 'a' to 'z':");
+        for (char ch = 'A'; ch <= 'z'; ch++) {
+            System.out.print(ch + " ");
+        }
+    }
+}
